f1eff10d573c431505efe064fc149f6f7752f468
User <moonchang04@naver.com>
2020-08-31 22:50:10 +0900

    제되로 되나 보자

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
-
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getDate();
+  }
+
+
+
+  void getLocation() async{
+   Location location = Location();
+   await location.getCurrentLocation();
+   print(location.longitude);
+   print(location.latitude);
+
+  }
+
+
+  void getDate()async{//비동기화 시켜야함
+
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    print(response.statusCode);
+
+    if(response.statusCode == 200 ){
+      String data = response.body;
+
+      var decodeDate = jsonDecode(data);
+      var temperature = decodeDate['main']['temp'];
+      var condition = decodeDate['weather'][0]['id'];
+      var cityName = decodeDate['name'];
+
+      
+    }else{
+      print(response.statusCode);
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+
+    return Scaffold();
+//앱이 실행되는 즉시 만들어야함
   }
+
 }
---
+++ lib/services/location.dart
@@ -0,0 +1,30 @@
+import 'package:geolocator/geolocator.dart';
+//location 리팩토링
+class Location{
+
+  double latitude ;
+ double longitude ;
+
+
+  Future<void> getCurrentLocation() async{
+    try {
+      Position position = await Geolocator().getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      latitude = position.longitude;
+      longitude= position.latitude;
+    }
+    catch(e){
+      print(e);
+    }
+  }
+}
+
+
+//try {
+//Position position = await Geolocator().getCurrentPosition(
+//desiredAccuracy: LocationAccuracy.low);
+//print(position);
+//}
+//catch(e){
+//print(e);
+//}
